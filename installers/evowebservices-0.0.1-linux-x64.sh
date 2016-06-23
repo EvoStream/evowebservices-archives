43a3c318c7b1f04cf5addd22ed229eeda0396d24
Toni Rose Panganiban <tpanganiban@evostream.com>
2016-06-23 23:44:29 +0800

    updated installers

--- installers/evowebservices-0.0.1-linux-x64.sh
+++ installers/evowebservices-0.0.1-linux-x64.sh
@@ -10,24 +10,36 @@ VER=4.1.1
 NODE_JS=http://nodejs.org/dist/v$VER/node-v$VER-$OS-$ARCH.tar.gz
 
 # Download and extract needed node JS
-curl $NODE_JS | tar xzf - --strip-components=1
-if [ $? != 0 ]; then echo "Could not download node binary: $NODE_JS!"; exit 1; fi
+which node
+if [ $? != 0 ]; then 
+    echo "Node.js DOES NOT exists.... Installing Node.js first...." 
+
+    wget $NODE_JS 
+
+    # Next, execute the following command to install the Node.js binary package in /usr/local/:
+    tar zxf node-v$VER-$OS-$ARCH.tar.gz -C /usr/local --strip-components 1 
+    if [ $? != 0 ]; then echo 'Could not install Node.js!'; exit 1; fi
+
+    
+else
+    echo "Node.js already exists" 
+fi 
 
 # Install dependencies
 echo 'Installing NPM evowebservices...'
 npm install --save https://github.com/EvoStream/evowebservices/tarball/master
-if [ $? != 0 ]; then echo 'Could not install ERS!'; exit 1; fi
-
-# Change to where evowebservices is
-cd ../node_modules/evowebservices
+if [ $? != 0 ]; then echo 'Could not install EVOWEBSERVICES!'; exit 1; fi
 
 echo 'Installing pm2...'
 npm install pm2 -g
 if [ $? != 0 ]; then echo 'Could not install pm2!'; exit 1; fi
 
+# Change to where evowebservices is
+cd node_modules/evowebservices
+
 # Start evowebservices
 echo 'Starting EVOWEBSERVICES...'
-pm2 start app.js
-if [ $? != 0 ]; then echo 'Could not start ERS!'; exit 1; fi
+npm start 
+if [ $? != 0 ]; then echo 'Could not start EVOWEBSERVICES!'; exit 1; fi
 
-exit 0
+exit 0;
--- installers/evowebservices-0.0.1-win-x64.bat
+++ installers/evowebservices-0.0.1-win-x64.bat
@@ -1,10 +1,10 @@
 @echo off
 
-echo creating installation folder in %SystemDrive%\node_evowebservices\.... 
-IF NOT EXIST %SystemDrive%\node_evowebservices\ MKDIR %SystemDrive%\node_evowebservices\
+echo creating installation folder in node_evowebservices\.... 
+IF NOT EXIST node_evowebservices MD node_evowebservices
 
-echo changing directory to %SystemDrive%\node_evowebservices\......
-CD %SystemDrive%\node_evowebservices\
+echo changing directory to node_evowebservices\......
+CD node_evowebservices
 
 echo getting evowebservices on github...
 call npm install --save https://github.com/EvoStream/evowebservices/tarball/master
@@ -12,32 +12,11 @@ call npm install --save https://github.com/EvoStream/evowebservices/tarball/mast
 echo changing directory to evowebservices....
 cd node_modules\evowebservices
 
-echo installing pm2....
-call npm install pm2 -g
+echo starting evowebservices using npm....
+call npm start
 
-echo starting evowebservices using pm2....
-call pm2 start app.js
-
-call pm2 list
 echo.
-echo.
-echo ---------------------------------------------------------------------
-echo Open your node command prompt and enter pm2 commands to check the status of running process of evowebservices
-echo.
-echo pm2 commands:
-echo List all running processes:
-echo call pm2 list
-echo.
-echo Stop an app:
-echo call pm2 stop 0
-echo.
-echo Restart an app:
-echo call pm2 restart 0
-echo.
-echo To view detailed information about an app:
-echo call pm2 show 0
-echo.
-echo To remove an app from PM2’s registry:
-echo call pm2 delete 0
+echo EVOWEBSERVICES INSTALLED ON %cd%\node_evowebservices\node_modules\evowebservices
+echo PRESS ANY KEY TO EXIT. 
 echo.
 pause >nul
